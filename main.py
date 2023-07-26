9369e738bae658dc9f5ad8baafa15cacd26214f2
Nandin Ganbat <95683647+Nandin01@users.noreply.github.com>
2023-07-26 16:35:19 +0200

    add: Initial setup files
    gitignore and requirements

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# To implement CRUD and fetch all the list
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
